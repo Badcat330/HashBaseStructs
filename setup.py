92982cc4d21d8d14260e5e06c100b457dc771e06
Alex <alextvrus@gmail.com>
2021-07-10 13:21:03 +0300

    Change setup file codestyle

--- setup.py
+++ setup.py
@@ -1,20 +1,20 @@
-from setuptools import find_packages, setup, version
+from setuptools import find_packages, setup
 
-setup (
-    name = 'hashbasestructs',
-    version = '0.1.0',
-    description = 'Here you can find Python3 lib with different hash structs for CDC task.',
-    author = 'Alex Glushko',
-    author_email = 'aglushko@hse.ru',
-    url = 'https://github.com/Badcat330/HashBaseStructs',
+setup(
+    name='hashBaseStructs',
+    version='0.1.0',
+    description='Here you can find Python3 lib with different hash structs for CDC task.',
+    author='Alex Glushko',
+    author_email='aglushko@hse.ru',
+    url='https://github.com/Badcat330/HashBaseStructs',
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
         "Development Status :: 1 - Planning"
     ],
-    package_dir = {"": "hashBaseStructs"},
-    packages = find_packages(where="hashBaseStructs"),
+    package_dir={"": "hashBaseStructs"},
+    packages=find_packages(where="hashBaseStructs"),
     python_requires=">=3.9",
     install_requires=[],
     setup_requires=['pytest-runner'],
